8afe15e37d61e916f3c5dea2f0959e0855f1d877
mrrahulkaran <karanr9991@gmail.com>
2023-07-05 16:05:13 +0530

    first commit

---
+++ firstprogramm/multiplie.java
@@ -0,0 +1,15 @@
+package firstprogramm;
+
+public class multiplie {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+	    
+		
+         for(int i=0; i>10; i++)
+ 
+	    System.out.println(i*10); 
+	}
+
+}
